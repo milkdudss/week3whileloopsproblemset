b0a7e47d3bc57a53991827d4b861facfb46a282f
milkdudss <albertkoshy@gmail.com>
2020-10-12 01:47:37 -0700

    for w

---
+++ whileloopproblemset/forloop.js
@@ -0,0 +1,18 @@
+for (let I = -10; I < 20; I++ ){
+  console.log(I);
+}
+for (let I = 10; I < 40; I++) {
+  if(I % 2 === 0) {
+    console.log(I)
+  }
+}
+for (let I = 301; I < 333; I+= 1) {
+  if(I % 2 != 0){
+    console.log(I)
+  }
+}
+for (let I = 10; I < 50; I++) {
+  if(I % 3 === 0 && I % 5 === 0) {
+    console.log(I)
+  }
+}
