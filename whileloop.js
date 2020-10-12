3b9d119bf86bc9115aa2ed82fec0d0a8fa80b886
milkdudss <albertkoshy@gmail.com>
2020-10-12 01:13:11 -0700

    while loop

---
+++ whileloop.js
@@ -0,0 +1,29 @@
+var Counter = -10
+
+while(Counter < 20) {
+  console.log(Counter);
+  Counter ++;
+}
+
+var Even = 10
+
+while(Even < 40) {
+  console.log(Even);
+  Even +=2;
+}
+
+var Odd = 301
+
+while(Odd < 333) {
+  console.log(Odd);
+  Odd +=2;
+}
+
+var Num = 5
+
+while(Num < 50) {
+  if (Num % 3 === 0 && Num % 5 === 0) {
+    console.log(Num)
+  }
+  Num ++
+}
